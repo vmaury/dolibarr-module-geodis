f2c4b51b97deb7286ecb6999ac452b922981ba40
vmaury <artec.vm@arnac.net>
2024-10-24 15:37:28 +0200

    WIP

--- core/modules/modGeodis.class.php
+++ core/modules/modGeodis.class.php
@@ -87,7 +87,8 @@ class modGeodis extends DolibarrModules
 		// If file is in theme/yourtheme/img directory under name object_pictovalue.png, use this->picto='pictovalue'
 		// If file is in module/img directory under name object_pictovalue.png, use this->picto='pictovalue@module'
 		// To use a supported fa-xxx css style of font awesome, use this->picto='xxx'
-		$this->picto = 'fa-file-o';
+		//$this->picto = 'fa-file-o';
+		$this->picto = 'geodis@geodis';
 
 		// Define some features supported by module (triggers, login, substitutions, menus, css, etc...)
 		$this->module_parts = array(
